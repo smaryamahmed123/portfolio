76b1f11226beace53e09574de752bdbda68b8e95
Syeda Maryam Ahmed <smaryamahmed2006@gmail.com>
2024-08-19 17:00:41 +0500

    commiting

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,7 @@ import { Container, Typography, Box, Grid, Card, CardContent, CardMedia, Button 
 import blood from '../assets/blood5.jpg' 
 import weather from '../assets/weather.jpeg'
 import calculator from '../assets/Calculator.png'
-import ecommerce from '../assets/OIP.jpeg' 
+import ecommerce from '../assets/img3.jpg' 
 import quiz from '../assets/OIP (1).jpeg' 
 
 
@@ -13,31 +13,31 @@ const projects = [
     {
         title: 'Blood Bank App',
         description: 'A MERN app for managing blood donations and donors. Technologies used include MongoDB, Express, React, and Node.js. I developed the frontend using React and the backend using Node.js and Express, with MongoDB as the database.',
-        imageUrl: 'src/assets/blood5.jpg',
+        imageUrl: blood,
         link: 'https://mellifluous-madeleine-b83366.netlify.app'
     },
     {
         title: 'Weather App',
         description: 'A React app that provides weather updates for any location. It uses the OpenWeatherMap API to fetch real-time weather data. I implemented the frontend using React and styled it with CSS.',
-        imageUrl: 'src/assets/weather.jpeg',
+        imageUrl: weather,
         link: 'https://react-weather-app-tan-psi.vercel.app'
     },
     {
         title: 'Calculator',
         description: 'A simple calculator built with React. This project demonstrates my skills in building functional components and managing state in a React application.',
-        imageUrl: 'src/assets/Calculator.png',
+        imageUrl: calculator,
         link: 'https://stirring-khapse-7b049b.netlify.app'
     },
     {
         title: 'E-commerce Website',
         description: 'An e-commerce website built using React. It includes features like product listing, cart management, and checkout. The backend is powered by Node.js and Express, with MongoDB as the database.',
-        imageUrl: 'src/assets/OIP.jpeg',
-        link: 'https://react-hecathon-saturday.vercel.app/'
+        imageUrl: ecommerce,
+        link: 'https://mern-stack-e-commerce-app.netlify.app/'
     },
     {
         title: 'Quiz App ',
         description: 'A quiz app built with React that allows users to take quizzes on various topics. It features multiple-choice questions, real-time scoring, and a leaderboard. The app is styled with CSS for a responsive design.',
-        imageUrl: 'src/assets/OIP (1).jpeg',
+        imageUrl: quiz,
         link: 'https://react-quiz-app-liard.vercel.app/'
     },
 ];
